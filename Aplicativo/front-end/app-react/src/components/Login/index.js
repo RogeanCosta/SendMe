92dd95785326d6243ecfcfb1e32f991c6b11694d
Alunos <Alunos>
2019-11-07 17:51:48 -0600

    Importada bootstrap

--- Aplicativo/front-end/app-react/src/components/Login/index.js
+++ Aplicativo/front-end/app-react/src/components/Login/index.js
@@ -1,4 +1,24 @@
 import React from 'react'
 import './Login.css'
 
-export default () => <div><button id="entrar">Entrar</button></div>
+export default () => (
+    <div>
+        <button id="entrar">Entrar</button>
+        <form>
+            <div class="form-group">
+                <label for="InputEmail">Email </label>
+                <input type="email"  class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                <small id="emailHelp" class="form-text text-muted">Digite o seu e-mail que foi cadastrado</small>
+            </div>
+            <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <br></br>
+            <input type="password"  class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            </div>
+        </form>
+
+
+    </div>
+
+);
+
