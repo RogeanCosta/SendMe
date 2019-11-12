e5da98ebef2c3b9f7b98485f3dbbe90b61f98215
Rogean Silva <rogean.ifce@gmail.com>
2019-11-12 13:29:12 -0300

    Adicionando a tela de cadastro com conexão através do router com a tela de login

--- Aplicativo/front-end/app-react/src/components/tela-cadastro/index.js
+++ Aplicativo/front-end/app-react/src/components/tela-cadastro/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Form, { FormRow } from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
