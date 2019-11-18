c56496cc21a34085e79dc90ce4e54e2552a69d2b
Rogean Silva <rogean.ifce@gmail.com>
2019-11-17 21:51:15 -0300

    Adicionando Tela de Produtos Primária

--- Aplicativo/front-end/app-react/src/components/App/index.js
+++ Aplicativo/front-end/app-react/src/components/App/index.js
@@ -4,6 +4,7 @@ import Login from "../Login";
 import Home from "../Home";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Cadastro from "../tela-cadastro"
+import Produtos from "../Produtos";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" exact={true} component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/cadastro" component={Cadastro} />
+          <Route path="/produtos" component={Produtos}/>
         </Switch>
       </BrowserRouter>
     </div>
