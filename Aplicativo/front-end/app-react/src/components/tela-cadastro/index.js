7c39e51a8d38585fc642e0e8a46d92cedd7ed45d
YasmimL <yasmimlima1601@gmail.com>
2019-11-18 21:28:56 -0300

    Resolução de conflitos.

--- Aplicativo/front-end/app-react/src/components/tela-cadastro/index.js
+++ Aplicativo/front-end/app-react/src/components/tela-cadastro/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css'
-import Form, { FormRow } from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
 
