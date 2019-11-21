44454e8728c6ce68f23df50b689c54559ee1d134
Vinicius <mviniciusc93@hotmail.com>
2019-11-20 23:55:14 -0300

    concluida tela

--- Aplicativo/front-end/app-react/src/components/Inicial/Categorias.js
+++ Aplicativo/front-end/app-react/src/components/Inicial/Categorias.js
@@ -5,33 +5,33 @@ const Categorias =[
     },
 
     {
-        "nome": "Fraldinha Bovina Maturatta 500g",
-        "preco": 9.59,
-        "imagem": "Imagens/Fraldinha.jpg"
+        "nome": "Descontos",
+        "imagem": "Imagens/promoçao.png"
     },
 
     {
-        "nome": "Manteiga Itambé 200g",
-        "preco": 5.99,
-        "imagem": "Imagens/Manteiga.jpg"
+        "nome": "Alimentos",
+        "imagem": "Imagens/alimentos.png"
     },
 
     {
-        "nome": "Feijão de Corda Kicaldo 1kg",
-        "preco": 4.99,
-        "imagem": "Imagens/feijaoDeCordaKicaldo.jpg" 
+        "nome": "Carnes, Aves, Peixes",
+        "imagem": "Imagens/carne.png" 
     },
 
     {
-        "nome": "Leite Integral Zero Lactose Piracanjuba 1L",
-        "preco": 4.39,
-        "imagem": "Imagens/leiteZeroLactosePiracanjuba.jpg"
+        "nome": "Frutas e Verduras",
+        "imagem": "Imagens/frutas.jpg"
     },
 
     {
-        "nome": "Macarrão Instântaneo Qualitá 85g Sabor Galinha Caipira",
-        "preco": 0.89,
-        "imagem": "Imagens/macarraoInstataneoQualita.jpg"
+        "nome": "Limpeza",
+        "imagem": "Imagens/limpeza.png"
+    },
+
+    {
+        "nome": "Pet",
+        "imagem": "imagens/pet.png"
     }
 ];
 
--- Aplicativo/front-end/app-react/src/components/Inicial/index.js
+++ Aplicativo/front-end/app-react/src/components/Inicial/index.js
@@ -13,121 +13,87 @@ export default class Produtos extends Component{
             <h1>Selecione a categoria</h1>
           </div>
           <div>
-            <CardDeck id="CardDeck">
+            <CardDeck className="CardDeck">
             
-            <Card className="card01">
+            <Card className="cartao">
               <Card.Body>
                 <Card.Title>{Category[0].nome}<br/></Card.Title>
               </Card.Body>
-              <Card.Img variant="top" src={Category[0].imagem}/>
+              <Card.Img className="imagem" src={Category[0].imagem}/>
              
             </Card>
             
-            <Card>
-              <Card.Img variant="top" src={Category[1].imagem}/>
+            <Card className="cartao">             
               <Card.Body>
-        <Card.Title>{Category[1].nome}</Card.Title>
-                <Card.Text id="Preco">
-                  R$ {Category[1].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
+                <Card.Title>{Category[1].nome}</Card.Title>
               </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
+              <Card.Img src={Category[1].imagem}/>
             </Card>
 
-            <Card>
-              <Card.Img variant="top" src={Category[2].imagem} />
+            <Card className="cartao">
               <Card.Body>
               <Card.Title>{Category[2].nome}</Card.Title>
-                <Card.Text>
-                  R$ {Category[2].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
               </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
+              <Card.Img src={Category[2].imagem} />
+            </Card>
+
+            <Card className="cartao">
+              <Card.Body>
+                <Card.Title>{Category[3].nome}</Card.Title>
+              </Card.Body>
+              <Card.Img src={Category[3].imagem} />
+            </Card>
+
+            <Card className="cartao">             
+              <Card.Body>
+                <Card.Title>{Category[4].nome}</Card.Title>
+              </Card.Body>
+              <Card.Img src={Category[4].imagem}/>
             </Card>
           </CardDeck>
           </div>
 
           <div>
-          <CardDeck id="CardDeck">
-            <Card>
-              <Card.Img variant="top" src={Category[3].imagem}/>
+            <CardDeck className="CardDeck">
+            
+            <Card className="cartao">
               <Card.Body>
-                <Card.Title>{Category[3].nome}<br/></Card.Title>
-                <Card.Text>
-                     R$ {Category[3].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                <i class="icon-minus-sign"></i>
-                <input type="tel" min="1" id="Quantity"></input>
-                <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
+                <Card.Title>{Category[0].nome}<br/></Card.Title>
               </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
+              <Card.Img src={Category[0].imagem}/>
+             
+            </Card>
+            
+            <Card className="cartao">             
+              <Card.Body>
+                <Card.Title>{Category[1].nome}</Card.Title>
+              </Card.Body>
+              <Card.Img src={Category[1].imagem}/>
             </Card>
 
-            <Card>
-              <Card.Img variant="top" src={Category[4].imagem}/>
+            <Card className="cartao">
               <Card.Body>
-        <Card.Title>{Category[4].nome}</Card.Title>
-                <Card.Text id="Preco">
-                  R$ {Category[4].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
+              <Card.Title>{Category[2].nome}</Card.Title>
               </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
+              <Card.Img src={Category[2].imagem} />
             </Card>
 
-            <Card>
-              <Card.Img id="Nissin" variant="top" src={Category[5].imagem} />
+            <Card className="cartao">
               <Card.Body>
-                <Card.Title>{Category[5].nome}</Card.Title>
-                <Card.Text>
-                  R$ {Category[5].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
+                <Card.Title>{Category[3].nome}</Card.Title>
               </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
+              <Card.Img src={Category[3].imagem} />
+            </Card>
+
+            <Card className="cartao">             
+              <Card.Body>
+                <Card.Title>{Category[4].nome}</Card.Title>
+              </Card.Body>
+              <Card.Img src={Category[4].imagem}/>
             </Card>
           </CardDeck>
           </div>
+
         </div>
         );
     }
