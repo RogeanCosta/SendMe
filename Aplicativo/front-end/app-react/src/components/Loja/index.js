17242a248ad15733b866bb819f0a798766da5c22
YasmimL <yasmimlima1601@gmail.com>
2019-11-24 20:14:17 -0300

    Integração da Tela de Loja/Subcategorias com o firebase.

--- Aplicativo/front-end/app-react/src/components/Loja/index.js
+++ Aplicativo/front-end/app-react/src/components/Loja/index.js
@@ -1,98 +1,38 @@
 import React,{Component} from 'react'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
-import Category from './Categorias.js'
+import categorias from './Categorias.js'
 import './inicial.css'
+import { Link } from 'react-router-dom'
+import chunk from "../../utils/chunk"
 
 export default class Produtos extends Component{
+  renderCategorias = () => {
+    const decks = chunk(categorias, 5);
+    
+    return decks.map(deck => (
+      <div>
+        <CardDeck className="CardDeck">
+          {deck.map(categoria => (
+            <Card className="cartao" key={categoria.tipo}>
+              <Card.Body>
+                <Link to={{ pathname: '/produtos', query: { categoria: categoria.tipo } }}><Card.Title>{categoria.nome}<br/></Card.Title></Link>
+              </Card.Body>
+              <Link to={{ pathname: '/produtos', query: { categoria: categoria.tipo } }}><Card.Img className="imagem" src={categoria.imagem}/></Link>            
+            </Card>
+          ))}
+        </CardDeck>
+      </div>
+    ));
+  }
+
     render(){
         return(
         <div>
           <div id="Apresentacao">
             <h1>Selecione a categoria</h1>
           </div>
-          <div>
-            <CardDeck className="CardDeck">
-            
-            <Card className="cartao">
-              <Card.Body>
-                <Card.Title>{Category[0].nome}<br/></Card.Title>
-              </Card.Body>
-              <Card.Img className="imagem" src={Category[0].imagem}/>
-             
-            </Card>
-            
-            <Card className="cartao">             
-              <Card.Body>
-                <Card.Title>{Category[1].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[1].imagem}/>
-            </Card>
-
-            <Card className="cartao">
-              <Card.Body>
-              <Card.Title>{Category[2].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[2].imagem} />
-            </Card>
-
-            <Card className="cartao">
-              <Card.Body>
-                <Card.Title>{Category[3].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[3].imagem} />
-            </Card>
-
-            <Card className="cartao">             
-              <Card.Body>
-                <Card.Title>{Category[4].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[4].imagem}/>
-            </Card>
-          </CardDeck>
-          </div>
-
-          <div>
-            <CardDeck className="CardDeck">
-            
-            <Card className="cartao">
-              <Card.Body>
-                <Card.Title>{Category[0].nome}<br/></Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[0].imagem}/>
-             
-            </Card>
-            
-            <Card className="cartao">             
-              <Card.Body>
-                <Card.Title>{Category[1].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[1].imagem}/>
-            </Card>
-
-            <Card className="cartao">
-              <Card.Body>
-              <Card.Title>{Category[2].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[2].imagem} />
-            </Card>
-
-            <Card className="cartao">
-              <Card.Body>
-                <Card.Title>{Category[3].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[3].imagem} />
-            </Card>
-
-            <Card className="cartao">             
-              <Card.Body>
-                <Card.Title>{Category[4].nome}</Card.Title>
-              </Card.Body>
-              <Card.Img src={Category[4].imagem}/>
-            </Card>
-          </CardDeck>
-          </div>
-
+          {this.renderCategorias()}
         </div>
         );
     }
--- Aplicativo/front-end/app-react/src/components/Produtos/index.js
+++ Aplicativo/front-end/app-react/src/components/Produtos/index.js
@@ -1,146 +1,98 @@
 import React,{Component} from 'react'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import Button from 'react-bootstrap/Button'
-import Product from './Produtos.js'
+import Alert from 'react-bootstrap/Alert'
 import './index.css'
+import firebase from "../../config/firebase"
+import chunk from "../../utils/chunk"
 
 export default class Produtos extends Component{
+  constructor(){
+    super();
+    this.db = firebase.database()
+  }
+  
+  state = {
+    produtos: []
+  }
+  
+  componentDidMount(){
+    this.db.ref('produtos').on("value", snapshot => {
+     
+      let produtos = snapshot.val();
+
+      if(this.props.location.query && this.props.location.query.categoria) {
+        const categoria = this.props.location.query.categoria;
+        if(categoria){
+          produtos = snapshot.val().filter(produto => produto.tipo === categoria);
+        }
+      }
+
+      this.setState({produtos})
+    });
+  }
+
+  renderProdutos = () => {
+    if (!this.state.produtos.length) {
+      return (
+        <div style={{marginTop: '10px'}}>
+        <Row>
+        <Col sm="10" className="offset-sm-1">
+          <Alert variant='warning'>
+            Nenhum produto para esta categoria!
+          </Alert>
+          </Col>
+        </Row>
+        </div>
+      )
+    }
+
+    const decks = chunk(this.state.produtos, 3);
+    
+    return decks.map(deck => (
+      <div>
+        <Row>
+        <CardDeck>
+          {deck.map(produto => (
+            <Col sm="4">
+              <Card>
+                <Card.Img variant="top" src={`/imagens/${produto.imagem}`}/>
+                <Card.Body>
+                  <Card.Title>{produto.nome}<br/></Card.Title>
+                  <Card.Text>
+                      R$ {produto.preco}
+                  </Card.Text>
+                  <div id="Quantidade">                
+                  <i className="icon-minus-sign"></i>
+                  <input type="tel" min="1" id="Quantity"></input>
+                  <i className="icon-plus-sign"></i>
+                  </div> 
+                  <br/>
+                  <i className="icon-heart-empty"></i>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant="success"><i className="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </CardDeck>
+        </Row>
+      </div>
+    ));
+  }
+
+
     render(){
         return(
         <div>
           <div id="Apresentacao">
             <h1>Supermercado São Luiz - Categoria: Mercearia</h1>
           </div>
-          <div>
-            <CardDeck id="CardDeck">
-            <Card>
-              <Card.Img variant="top" src={Product[0].imagem}/>
-              <Card.Body>
-                <Card.Title>{Product[0].nome}<br/></Card.Title>
-                <Card.Text>
-                     R$ {Product[0].preco}
-                </Card.Text>
-                <div id="Quantidade">                
-                <i class="icon-minus-sign"></i>
-                <input type="tel" min="1" id="Quantity"></input>
-                <i class="icon-plus-sign"></i>
-                </div> 
-                <br/>
-                <i class="icon-heart-empty"></i>
-
-                
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
-            </Card>
-
-            <Card>
-              <Card.Img variant="top" src={Product[1].imagem}/>
-              <Card.Body>
-        <Card.Title>{Product[1].nome}</Card.Title>
-                <Card.Text id="Preco">
-                  R$ {Product[1].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
-            </Card>
-
-            <Card>
-              <Card.Img variant="top" src={Product[2].imagem} />
-              <Card.Body>
-              <Card.Title>{Product[2].nome}</Card.Title>
-                <Card.Text>
-                  R$ {Product[2].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
-          </div>
-
-          <div>
-          <CardDeck id="CardDeck">
-            <Card>
-              <Card.Img variant="top" src={Product[3].imagem}/>
-              <Card.Body>
-                <Card.Title>{Product[3].nome}<br/></Card.Title>
-                <Card.Text>
-                     R$ {Product[3].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                <i class="icon-minus-sign"></i>
-                <input type="tel" min="1" id="Quantity"></input>
-                <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
-            </Card>
-
-            <Card>
-              <Card.Img variant="top" src={Product[4].imagem}/>
-              <Card.Body>
-        <Card.Title>{Product[4].nome}</Card.Title>
-                <Card.Text id="Preco">
-                  R$ {Product[4].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
-            </Card>
-
-            <Card>
-              <Card.Img id="Nissin" variant="top" src={Product[5].imagem} />
-              <Card.Body>
-                <Card.Title>{Product[5].nome}</Card.Title>
-                <Card.Text>
-                  R$ {Product[5].preco}
-                </Card.Text>
-                <div id="Quantidade">
-                  <i class="icon-minus-sign"></i>
-                  <input type="tel" min="1" id="Quantity"></input>
-                  <i class="icon-plus-sign"></i>
-                </div>
-                <br/>
-                <i class="icon-heart-empty"></i>
-              </Card.Body>
-              <Card.Footer>
-                <Button variant="success"><i class="icon-shopping-cart"></i> Adicionar ao Carrinho</Button>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
-          </div>
+          {this.renderProdutos()}
         </div>
         );
     }
