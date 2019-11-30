902f01365876bd5801bb2fb66a977a52203234cc
Vinicius <mviniciusc93@hotmail.com>
2019-11-30 19:10:37 -0300

    tela finalizada

--- Aplicativo/front-end/app-react/src/components/Dados/index.js
+++ Aplicativo/front-end/app-react/src/components/Dados/index.js
@@ -34,7 +34,7 @@ export default class Cadastro extends React.Component {
         <div id="Container" >
 
 
-        <div class="panel-group01">
+        <div class="panel-group">
           <div class="painel-dados" id="painel-esquerdo">
            <div class="panel-heading">Nome completo</div>
            <div class="panel-body">dado do firebase</div>
@@ -48,7 +48,7 @@ export default class Cadastro extends React.Component {
         </div>
 
         <div id="Container">
-          <div class="panel-group01">
+          <div class="panel-group">
             <div class="painel-dados" id="painel-esquerdo">
               <div class="panel-heading">RG</div>
               <div class="panel-body">dado do firebase</div>
@@ -65,68 +65,57 @@ export default class Cadastro extends React.Component {
 
           <div class="panel-group">
             <div class="painel-dados" id="painel-esquerdo">
-              <div class="panel-heading">CEP</div>
-              <div class="panel-body">CEP atual (dado do firebase)</div>
+              <div class="panel-heading">Endereço</div>
+              <div class="panel-body">Endereço atual (dado do firebase)</div>
               <div class="panel-body">
                 <Form.Control type="text" id="form-nome" placeholder="Digite seu novo CEP" />                  
               </div>
             </div>
           </div>
         </div>
+
         <div id="Container">
-
-        <div class="panel-group">
-          <div class="painel-dados" id="painel-dados">
-            <div class="panel-heading">Endereço</div>
-            <div class="panel-body">Endereço atual (dado do firebase)</div>
-            <div class="panel-body">
-              <Form.Control type="text" id="form-nome" placeholder="Digite seu novo endereço" />                  
-            </div>
-          </div>
-          </div>
-
           <div class="panel-group">
-          <div class="painel-dados" id="painel-dados">
-            <div class="panel-heading">Panel Header</div>
-            <div class="panel-body">Panel Content</div>
-            <div class="panel-body">
-              <Form.Control type="text" id="form-nome" placeholder="Digite seu nome" />                  
+            <div class="painel-dados" id="painel-cep">
+              <div class="panel-heading">CEP</div>
+              <div class="panel-body">CEP atual (dado do firebase)</div>
+              <div class="panel-body">
+                <Form.Control type="text" id="form-nome" placeholder="Digite seu novo endereço" />                  
+              </div>
+            </div>
+
+            <div class="painel-dados" id="painel-numero">
+              <div class="panel-heading">Número</div>
+              <div class="panel-body">Número atual (dado do firebase)</div>
+              <div class="panel-body">
+                <Form.Control type="text" id="form-nome" placeholder="Digite o novo número" />
+              </div>
+            </div>
+
+            <div class="painel-dados" id="numero">
+              <div class="panel-heading">Complemento</div> 
+              <div class="panel-body">Complemento atual (dado do firebase)</div>
+              <div class="panel-body">
+                <Form.Control type="text" id="form-nome" placeholder="Digite o novo complemento" />                  
+              </div>
             </div>
           </div>
-          </div>
+        </div>
 
+        <div id="Container">
           <div class="panel-group">
-          <div class="painel-dados" id="painel-dados">
-            <div class="panel-heading">Panel Header</div>
-            <div class="panel-body">Panel Content</div>
-            <div class="panel-body">
-              <Form.Control type="text" id="form-nome" placeholder="Digite seu nome" />                  
+            <div class="painel-dados" id="painel-esquerdo">
+              <div class="panel-heading">Cidade</div>
+              <div class="panel-body">Cidade atual (dado do firebase)</div>
+              <div class="panel-body">
+                <Form.Control type="text" id="form-nome" placeholder="Digite a nova cidade" />                  
+              </div>
             </div>
-          </div>
-          </div>
 
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridEndereco">
-              <Form.Label id="RogeanLabels">Endereço</Form.Label>
-              <Form.Control placeholder="Rua, Avenida..." />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridComplemento">
-              <Form.Label id="RogeanLabels">Complemento</Form.Label>
-              <Form.Control placeholder="Bloco, Apt., Casa..." />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridNumero">
-              <Form.Label id="RogeanLabels">Numero</Form.Label>
-              <Form.Control placeholder="Ex. 1245" />
-            </Form.Group>
-          </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridCidade">
-              <Form.Label id="RogeanLabels">Cidade</Form.Label>
-              <Form.Control placeholder="Digite a sua Cidade" />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridEstado">
-              <Form.Label id="RogeanLabels">Estado</Form.Label>
+            <div class="painel-dados" id="estado">
+              <div class="panel-heading">Estado</div>
+              <div class="panel-body">Estado atual (dado do firebase)</div>
+              <div class="panel-body">
               <Form.Control as="select">
                 <option>Escolha seu Estado</option>
                 <option>Acre</option>
@@ -155,43 +144,39 @@ export default class Cadastro extends React.Component {
                 <option>São Paulo</option>
                 <option>Sergipe</option>
                 <option>Tocantins</option>
-              </Form.Control>
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridTel">
-              <Form.Label id="RogeanLabels">Telefone 01</Form.Label>
-              <Form.Control type="text" placeholder="Digite seu Telefone" />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridTel">
-              <Form.Label id="RogeanLabels">Telefone 02</Form.Label>
-              <Form.Control type="text" placeholder="Digite seu Telefone" />
-            </Form.Group>
-          </Form.Row>
-
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label id="RogeanLabels">Email</Form.Label>
-              <Form.Control
-                name="email"
-                type="email"
-                placeholder="Digite seu Email"
-                onChange={this.handleChange}
-              />
-            </Form.Group>
-          </Form.Row>
-
-          <div class="panel-group">
-          <div class="painel-dados" id="painel-dados">
-            <div class="panel-heading">Panel Header</div>
-            <div class="panel-body">Panel Content</div>
-            <div class="panel-body">
-              <Form.Control type="text" id="form-nome" placeholder="Digite seu nome" />                  
+              </Form.Control>                  
+              </div>
             </div>
           </div>
+        </div>
+
+        <div id="Container">
+          <div class="panel-group">
+            <div class="painel-dados" id="painel-esquerdo">
+              <div class="panel-heading">Telefone 01</div>
+              <div class="panel-body">Telefone atual (dado do firebase)</div>
+              <div class="panel-body">
+                <Form.Control type="text" id="form-nome" placeholder="Digite o novo número" />                  
+              </div>
+            </div>
+
+            <div class="painel-dados" >
+              <div class="panel-heading">Telefone 02</div>
+              <div class="panel-body">Telefone atual (dado do firebase)</div>
+              <div class="panel-body">
+                <Form.Control type="text" id="form-nome" placeholder="Digite o novo número" />                  
+              </div>
+            </div>
           </div>
+        </div>
+
+        <div id="Container">
+           <div class="panel-group">
+            <div class="painel-dados" id="painel-esquerdo">
+              <div class="panel-heading">Email</div>
+              <div class="panel-body">dado do firebase</div>
+            </div>
+           </div>
 
           <div id="buttons" className="buttons">
             <Button id="cadastrar" variant="success" type="submit">
