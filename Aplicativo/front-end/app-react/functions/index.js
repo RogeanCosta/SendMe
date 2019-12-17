b7a4aa3e5b7fc9bdb8d94f144997aef5208eec27
Rogean Silva <rogean.ifce@gmail.com>
2019-12-17 19:00:01 -0300

    O que eu fiz para enviar o email

--- Aplicativo/front-end/app-react/functions/index.js
+++ Aplicativo/front-end/app-react/functions/index.js
@@ -1,4 +1,44 @@
-const functions = require('firebase-functions');
+'use strict';
+const functions  = require('firebase-functions');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+
+let url = {    
+    service: 'gmail',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'xtrsmk@gmail.com',
+        pass: 'MISERICORDIA123'
+    }
+};
+let transporter = nodemailer.createTransport(url);
+
+exports.enviarEmail = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    let remetente = '"Adson Rocha" <xtrsmk@gmail.com>';
+
+    let assunto = req.body['assunto'];
+    let destinatarios = req.body['destinatarios']; // lista de e-mails destinatarios separados por ,
+    let corpo = req.body['corpo'];
+    let corpoHtml = req.body['corpoHtml'];
+
+    let email = {
+        from: remetente,
+        to: destinatarios,
+        subject: assunto,
+        text: corpo,
+        html: corpoHtml
+    };
+
+    transporter.sendMail(email, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Mensagem %s enviada: %s', info.messageId, info.response);
+    });
+  });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
